8fb005f9471981384ea9481ce447a5e9feffff41
elyes <elyesbarnat42@gmail.com>
2017-03-12 22:29:00 +0100

    main essai pour tester

---
+++ Essai.java
@@ -0,0 +1,19 @@
+public class Essai {
+	public static void main(String[] args) {
+		Partie p = new Partie();
+		Joueur j1 = new Joueur("Amir");
+		Joueur j2 = new Joueur("Nadia");
+		j1.lancerDuel(p, j2);
+//		ArrayList<Joueur> j = new ArrayList<Joueur>();
+//		Joueur j1 = new Joueur("Amir");
+//		Joueur j2 = new Joueur("Amira");
+//		j.add(j1);
+//		j.add(j2);
+//		for (Joueur j3 : j)
+//			System.out.println(j3.getNom());
+//		Collections.shuffle(j);
+//		for (Joueur j3 : j)
+//			System.out.println(j3.getNom());
+		
+	}
+}
